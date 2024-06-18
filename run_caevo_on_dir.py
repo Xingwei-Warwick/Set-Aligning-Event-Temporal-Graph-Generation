a7cd730f7fea5f0fffa24fa212d487bddb8c0aa7
Xingwei-Warwick <Xingwei.Tan@warwick.ac.uk>
2024-06-18 13:41:27 +0100

    dir bug fixed

--- run_caevo_on_dir.py
+++ run_caevo_on_dir.py
@@ -1,7 +1,8 @@
 from os import listdir
 from argparse import ArgumentParser
-from os.path import isfile
+from os.path import isfile, join
 import subprocess
+import os
 
 
 if __name__ == "__main__":
@@ -14,22 +15,26 @@ if __name__ == "__main__":
             help="output directory, need to be created first")
     args = parser.parse_args()
 
-    file_list = listdir(args.input_dir)
+    abs_input_dir = os.path.abspath(args.input_dir)
+    abs_out_dir = os.path.abspath(args.out_dir)
+
+    os.chdir(args.caevo_path)
+
+    file_list = listdir(abs_input_dir)
 
     for idx, file in enumerate(file_list):
         if file.split('.')[-1] == 'txt':
-            command = f"{args.caevo_path}/runcaevoraw.sh {args.input_dir}/{file}"
+            command = f"./runcaevoraw.sh {abs_input_dir}/{file}"
+
             process = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
     
             output, error = process.communicate()
 
             error = error.decode("utf-8")
 
-            print(error)
-
             new_file_name = f"{file}.info.xml"
-            if isfile(f"{args.caevo_path}/{new_file_name}"):
-                command = f"mv {args.caevo_path}/{new_file_name} {args.out_dir}/"
+            if isfile(f"{abs_input_dir}/{new_file_name}"):
+                command = f"mv {abs_input_dir}/{new_file_name} {abs_out_dir}/"
                 process = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
                 print(f"{file} finished! {len(file_list)-idx-1} left")
             else:
